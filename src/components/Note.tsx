186ddc6898b81710fd2128869c64fe5088351073
Kevin Nguyen <ktn1234@yahoo.com>
2024-02-17 21:07:33 -0600

    :sparkles: Created Note component

---
+++ src/components/Note.tsx
@@ -0,0 +1,40 @@
+import { FaTrash } from "react-icons/fa";
+
+export interface Note {
+  id: string;
+  text: string;
+  date: string;
+}
+
+export interface NoteProps {
+  id: string;
+  text: string;
+  date: string;
+  notes: Note[];
+  setNotes: React.Dispatch<React.SetStateAction<Note[]>>;
+}
+
+function Note({ id, text, date, notes, setNotes }: NoteProps) {
+  const handleDeleteNote = (id: string) => {
+    const newNotes = notes.filter((note) => note.id !== id);
+    setNotes(newNotes);
+  };
+
+  return (
+    <div
+      key={id}
+      className="flex flex-col justify-between text-quaternary bg-secondary rounded-lg p-3 md:p-5 min-h-52 mb-5"
+    >
+      <span className="break-words">{text}</span>
+      <div className="flex items-center justify-between">
+        <small>{date}</small>
+        <FaTrash
+          className="cursor-pointer"
+          onClick={() => handleDeleteNote(id)}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Note;
