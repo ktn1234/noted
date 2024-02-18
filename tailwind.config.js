fc55212eb768779020392916270b5154043b11fc
Kevin Nguyen <ktn1234@yahoo.com>
2024-02-17 21:06:04 -0600

    :wrench: Added color palette to colors config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      primary: "#04364A",
+      secondary: "#176B87",
+      tertiary: "#64CCC5",
+      quaternary: "#DAFFFB",
+    },
     extend: {},
   },
   plugins: [],
