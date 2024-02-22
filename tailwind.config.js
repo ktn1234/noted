97cf67a8a9a198262bea31dc3f61472d339e925e
Kevin Nguyen <ktn1234@yahoo.com>
2024-02-22 16:09:01 -0600

    :bulb: Comment where color palette was acquired

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ export default {
   darkMode: "class",
   theme: {
     extend: {
+      // Color palette acquired from https://colorhunt.co/palette/04364a176b8764ccc5dafffb
       colors: {
         primary: "#04364A",
         secondary: "#176B87",
