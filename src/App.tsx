131b13084653fe40d30bbebe7c2271edba295d43
Kevin Nguyen <ktn1234@yahoo.com>
2024-02-23 02:10:10 -0600

    :recycle: Apply ThemeProvider, AuthProvider, and react router dom with protected paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,44 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { ThemeProvider } from "./contexts/ThemeContext";
+import { AuthProvider } from "./contexts/AuthContext";
+
+import RootLayout from "./components/RootLayout";
 import HomePage from "./pages/HomePage";
+import AuthPage from "./pages/AuthPage";
+import Protected from "./components/Protected";
+import SettingsPage from "./pages/SettingsPage";
 
 function App() {
-  return <HomePage />;
+  return (
+    <ThemeProvider>
+      <AuthProvider>
+        <BrowserRouter>
+          <RootLayout>
+            <Routes>
+              <Route path="/auth" element={<AuthPage />} />
+              <Route
+                index
+                element={
+                  <Protected>
+                    <HomePage />
+                  </Protected>
+                }
+              />
+              <Route
+                path="/settings"
+                element={
+                  <Protected>
+                    <SettingsPage />
+                  </Protected>
+                }
+              />
+              <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+          </RootLayout>
+        </BrowserRouter>
+      </AuthProvider>
+    </ThemeProvider>
+  );
 }
 
 export default App;
