a99c799d1adbb0022296b80b6ad445280b70cdb2
Kevin Nguyen <ktn1234@yahoo.com>
2024-02-23 01:58:49 -0600

    :sparkles: Created FormInput component

---
+++ src/components/FormInput.tsx
@@ -0,0 +1,43 @@
+interface FormInputProps {
+  label: string;
+  htmlFor: string;
+  type: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  value?: string;
+  placeholder?: string;
+  autoComplete?: string;
+  disabled?: boolean;
+  required?: boolean;
+}
+
+function FormInput({
+  label,
+  htmlFor,
+  type,
+  onChange,
+  value,
+  placeholder,
+  autoComplete,
+  disabled,
+  required,
+}: FormInputProps): JSX.Element {
+  return (
+    <div className="bg-tertiary dark:bg-secondary rounded-md p-2 mb-5">
+      <label className="text-xs" htmlFor={htmlFor}>
+        {label}
+      </label>
+      <input
+        className="bg-tertiary dark:bg-secondary text-primary dark:text-quaternary w-full border-none focus:outline-none placeholder:text-gray-500 dark:placeholder:text-[#9CA3AF]"
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        required={required}
+        onChange={onChange}
+        autoComplete={autoComplete}
+        disabled={disabled}
+      />
+    </div>
+  );
+}
+
+export default FormInput;
