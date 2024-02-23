7fb59e29a78c83ae01c383a29d616639c4df5e23
Kevin Nguyen <ktn1234@yahoo.com>
2024-02-23 02:38:13 -0600

    :recycle: :wrench: Refactor to use exported config vars

--- src/lib/supabase/index.ts
+++ src/lib/supabase/index.ts
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 import { Database } from "./types";
+import config from "../../config";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
-export default createClient<Database>(supabaseUrl, supabaseAnonKey);
+export default createClient<Database>(
+  config.SUPABASE_URL,
+  config.SUPABASE_ANON_KEY
+);
