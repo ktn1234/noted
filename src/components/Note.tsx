12742576fdfcb3b1c45a11c1432fcf6ebf8e9e88
Kevin Nguyen <ktn1234@yahoo.com>
2024-02-23 02:03:30 -0600

    :recycle: Re-work Note to use supabase types for notes table

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,33 +1,45 @@
 import { FaTrash } from "react-icons/fa";
-
-export interface Note {
-  id: string;
-  text: string;
-  date: string;
-}
+import { Tables } from "../lib/supabase/types";
+import supabase from "../lib/supabase";
 
 export interface NoteProps {
-  id: string;
+  id: number;
   text: string;
   date: string;
-  notes: Note[];
-  setNotes: React.Dispatch<React.SetStateAction<Note[]>>;
+  notes: Tables<"notes">[];
+  setNotes: React.Dispatch<React.SetStateAction<Tables<"notes">[]>>;
 }
 
 function Note({ id, text, date, notes, setNotes }: NoteProps) {
-  const handleDeleteNote = (id: string) => {
-    const newNotes = notes.filter((note) => note.id !== id);
-    setNotes(newNotes);
-  };
+  async function handleDeleteNote(id: number) {
+    try {
+      const { error } = await supabase
+        .from("notes")
+        .delete()
+        .eq("id", id)
+        .single<Tables<"notes">>();
+
+      if (error) {
+        console.error("[ERROR] Error deleting note:", error);
+        return;
+      }
+
+      setNotes(notes.filter((note) => note.id !== id));
+    } catch (error) {
+      console.error("[ERROR] Error deleting note:", error);
+    }
+  }
+
+  const dateString = new Date(date).toLocaleString();
 
   return (
     <div
       key={id}
-      className="flex flex-col justify-between text-primary dark:text-quaternary bg-tertiary dark:bg-secondary rounded-lg p-3 md:p-5 min-h-52 mb-5"
+      className="flex flex-col justify-between text-primary dark:text-quaternary bg-tertiary dark:bg-secondary rounded-lg p-3 md:p-5 min-h-52"
     >
       <span className="break-words">{text}</span>
       <div className="flex items-center justify-between">
-        <small>{date}</small>
+        <small>{dateString}</small>
         <FaTrash
           className="cursor-pointer"
           onClick={() => handleDeleteNote(id)}
