be317f0d9b1f4402e62003e43ac8e6c98a6dfcd0
Kevin Nguyen <ktn1234@yahoo.com>
2024-02-23 02:01:48 -0600

    :recycle: :sparkles: Re-work Navbar to include drop down menu of authenticated user and avatar picture

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,42 +1,80 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { FaStickyNote } from "react-icons/fa";
 import { MdLightMode, MdDarkMode } from "react-icons/md";
+import { IoMdPerson } from "react-icons/io";
 
-type Theme = "dark" | "light";
+import useAuth from "../hooks/useAuth";
+import useTheme from "../hooks/useTheme";
+
+import supabase from "../lib/supabase";
+import { useNavigate } from "react-router-dom";
+import LoadingPage from "../pages/LoadingPage";
 
 function Navbar(): JSX.Element {
-  const [theme, setTheme] = useState<Theme>("dark");
+  const navigate = useNavigate();
+  const { user } = useAuth();
+  const { theme, toggleTheme } = useTheme();
+  const [loading, setLoading] = useState(true);
+  const [avatar_url, setAvatarUrl] = useState<string | null>(null);
+  const [displayDropdown, setDisplayDropdown] = useState<boolean>(false);
+  const dropDownRef = useRef<HTMLDivElement | null>(null);
+
+  function signOut() {
+    supabase.auth.signOut();
+  }
 
   useEffect(() => {
-    const localTheme = localStorage.getItem("theme");
-    if (!localTheme) {
-      localStorage.setItem("theme", "dark");
-      return;
+    async function getAvatar() {
+      if (user) {
+        const { data, error } = await supabase
+          .from("profiles")
+          .select("avatar_url")
+          .eq("id", user.id)
+          .single();
+
+        if (error) {
+          console.error("[ERROR] Error getting avatar:", error);
+          return;
+        }
+
+        if (data) {
+          console.log("[LOG] Avatar data:", data);
+          setAvatarUrl(data.avatar_url);
+        }
+      }
+      setLoading(false);
     }
 
-    if (localTheme === "light") {
-      document.documentElement.classList.remove("dark");
-    }
-
-    setTheme(localTheme as Theme);
+    getAvatar();
   }, []);
 
-  const handleToggleTheme = () => {
-    if (theme === "dark") {
-      localStorage.setItem("theme", "light");
-      document.documentElement.classList.remove("dark");
-      setTheme("light");
-    } else {
-      localStorage.setItem("theme", "dark");
-      document.documentElement.classList.add("dark");
-      setTheme("dark");
+  useEffect(() => {
+    function closeDropdown(event: MouseEvent) {
+      if (
+        displayDropdown &&
+        dropDownRef.current &&
+        !dropDownRef.current.contains(event.target as Node)
+      ) {
+        setDisplayDropdown(false);
+      }
     }
-  };
+
+    document.addEventListener("mousedown", closeDropdown);
+
+    return () => {
+      document.removeEventListener("mousedown", closeDropdown);
+    };
+  }, [displayDropdown]);
+
+  if (loading) return <LoadingPage />;
 
   return (
     <nav className="flex items-center justify-between">
       <div>
-        <div className="flex items-center">
+        <div
+          className="flex items-center hover:underline cursor-pointer"
+          onClick={() => navigate("/")}
+        >
           <FaStickyNote />
           <span className="pl-2">Noted</span>
         </div>
@@ -44,17 +82,60 @@ function Navbar(): JSX.Element {
       <div className="flex gap-2 items-center">
         {theme === "dark" && (
           <MdLightMode
-            onClick={handleToggleTheme}
-            className="cursor-pointer"
+            onClick={toggleTheme}
+            className="cursor-pointer dark:hover:text-quaternary"
             size="1.5em"
           />
         )}
         {theme === "light" && (
           <MdDarkMode
-            onClick={handleToggleTheme}
-            className="cursor-pointer"
+            onClick={toggleTheme}
+            className="cursor-pointer hover:text-secondary"
             size="1.5em"
           />
+        )}
+        {user && (
+          <div ref={dropDownRef}>
+            {avatar_url && (
+              <img
+                src={avatar_url}
+                alt="Avatar"
+                onClick={() => setDisplayDropdown(!displayDropdown)}
+                className="w-8 h-8 rounded-full cursor-pointer"
+              />
+            )}
+            {!avatar_url && (
+              <IoMdPerson
+                onClick={() => setDisplayDropdown(!displayDropdown)}
+                className="cursor-pointer hover:text-secondary focus:text-secondary dark:hover:text-quaternary dark:focus:text-quaternary"
+                size="1.5em"
+              />
+            )}
+            {displayDropdown && (
+              <div className="absolute right-0 flex flex-col mt-2 mr-5 rounded-md bg-tertiary text-primary dark:bg-secondary dark:text-quaternary shadow-lg max-w-44">
+                <ul className="py-2">
+                  <li className="px-4 py-2">
+                    <span className="break-words">{user.email}</span>
+                  </li>
+                  <li
+                    className="px-4 py-2 dark:hover:text-primary hover:text-quaternary cursor-pointer"
+                    onClick={() => {
+                      navigate("/settings");
+                      setDisplayDropdown(false);
+                    }}
+                  >
+                    <span>Settings</span>
+                  </li>
+                  <li
+                    className="px-4 py-2 dark:hover:text-primary hover:text-quaternary cursor-pointer"
+                    onClick={signOut}
+                  >
+                    <span>Sign out</span>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
         )}
       </div>
     </nav>
