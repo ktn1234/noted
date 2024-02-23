b8defdf2b0041d28203da631b3b68cf2adb5a32c
Kevin Nguyen <ktn1234@yahoo.com>
2024-02-23 02:37:45 -0600

    :wrench: Created config file for parsing and exporting public env vars

---
+++ src/config/index.ts
@@ -0,0 +1,7 @@
+const config = {
+  SUPABASE_URL: import.meta.env.VITE_SUPABASE_URL,
+  SUPABASE_ANON_KEY: import.meta.env.VITE_SUPABASE_ANON_KEY,
+  SITE_URL: import.meta.env.VITE_SITE_URL,
+};
+
+export default config;
