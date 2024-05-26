cd88d9cde9b063a4edf5c1cbc18094bcaa01f8d3
Kevin Nguyen <ktn1234@yahoo.com>
2024-05-26 17:38:33 -0500

    :fire: Remove unused SITE_URL config env var

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,6 @@
 const config = {
   SUPABASE_URL: import.meta.env.VITE_SUPABASE_URL,
   SUPABASE_ANON_KEY: import.meta.env.VITE_SUPABASE_ANON_KEY,
-  SITE_URL: import.meta.env.VITE_SITE_URL,
 };
 
 export default config;
