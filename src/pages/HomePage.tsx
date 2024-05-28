97d459cbda72aa3c98c6667ca2c75da24b0f9437
Kevin Nguyen <ktn1234@yahoo.com>
2024-05-27 19:50:38 -0500

    :sparkles: Refresh Notes

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { IoMdRefresh } from "react-icons/io";
 
 import useAuth from "../hooks/useAuth";
 
@@ -21,6 +22,7 @@ function HomePage(): JSX.Element {
   const [searchText, setSearchText] = useState<string>("");
   const [modalText, setModalText] = useState<string>("");
   const [showModal, setShowModal] = useState<boolean>(false);
+  const [isRefreshing, setIsRefreshing] = useState<boolean>(false);
 
   useEffect(() => {
     if (!user) return;
@@ -35,6 +37,23 @@ function HomePage(): JSX.Element {
         setLoading(false);
       });
   }, [user]);
+
+  async function handleRefreshNotes() {
+    setIsRefreshing(true);
+    const { data, error } = await supabase
+      .from("notes")
+      .select("*, profiles (*)")
+      .order("created_at", { ascending: false });
+
+    if (error) {
+      console.error("[ERROR] Error fetching notes", error);
+    }
+
+    if (data) {
+      setNotes(data);
+    }
+    setIsRefreshing(false);
+  }
 
   async function handleSaveNote(text: string) {
     if (text.trim().length === 0) {
@@ -96,35 +115,48 @@ function HomePage(): JSX.Element {
           <h1 className="text-2xl text-center">All Notes</h1>
           <section className="flex flex-col gap-2 w-full md:w-[50%]">
             <AddNote handleSaveNote={handleSaveNote} />
-            <div className="mt-5">
-              <Search handleSearch={handleSearch} />
+            <div className="flex justify-normal items-center mt-5">
+              <span className="w-full">
+                <Search handleSearch={handleSearch} />
+              </span>
+              <span className="mx-5">
+                <IoMdRefresh
+                  className={`cursor-pointer rounded-full p-1 border-2 border-tertiary text-secondary hover:bg-tertiary hover:text-primary dark:text-tertiary dark:border-secondary dark:hover:text-quaternary dark:hover:bg-secondary ${
+                    isRefreshing && "animate-spin"
+                  }`}
+                  size="40"
+                  onClick={handleRefreshNotes}
+                />
+              </span>
             </div>
-            {notes
-              .filter((note) =>
-                note.text.toLowerCase().includes(searchText.toLowerCase())
-              )
-              .map((note) =>
-                note.profiles?.user_id === profile?.user_id ? (
-                  <Note
-                    key={note.id}
-                    id={note.id}
-                    text={note.text}
-                    username={note.profiles!.username as string}
-                    avatar_url={note.profiles!.avatar_url as string}
-                    date={note.created_at}
-                    handleDeleteNote={handleDeleteNote}
-                  />
-                ) : (
-                  <Note
-                    key={note.id}
-                    id={note.id}
-                    text={note.text}
-                    username={note.profiles!.username as string}
-                    avatar_url={note.profiles!.avatar_url as string}
-                    date={note.created_at}
-                  />
+            {isRefreshing && <LoadingPage />}
+            {!isRefreshing &&
+              notes
+                .filter((note) =>
+                  note.text.toLowerCase().includes(searchText.toLowerCase())
                 )
-              )}
+                .map((note) =>
+                  note.profiles?.user_id === profile?.user_id ? (
+                    <Note
+                      key={note.id}
+                      id={note.id}
+                      text={note.text}
+                      username={note.profiles!.username as string}
+                      avatar_url={note.profiles!.avatar_url as string}
+                      date={note.created_at}
+                      handleDeleteNote={handleDeleteNote}
+                    />
+                  ) : (
+                    <Note
+                      key={note.id}
+                      id={note.id}
+                      text={note.text}
+                      username={note.profiles!.username as string}
+                      avatar_url={note.profiles!.avatar_url as string}
+                      date={note.created_at}
+                    />
+                  )
+                )}
           </section>
           {showModal && (
             <Modal
