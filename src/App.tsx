70401a9dbd6aee29af810e9648ef01d7932e189e
Kevin Nguyen <ktn1234@yahoo.com>
2024-06-26 23:52:21 -0500

    :sparkles: Wrap entire app with QueryClientProvider and added ReactQueryDevTools component

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import {
   Route,
   Routes,
 } from "react-router-dom";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import ThemeProvider from "./contexts/ThemeProvider";
 import AuthProvider from "./contexts/AuthProvider";
@@ -23,35 +25,43 @@ import ProfilePage from "./pages/ProfilePage";
 import SettingsPage from "./pages/SettingsPage";
 import NotificationsPage from "./pages/NotificationsPage";
 
+import queryclient from "./lib/react-query";
+
 function App() {
   return (
     <ThemeProvider>
       <AuthProvider>
         <BrowserRouter>
-          <RootLayout>
-            <Routes>
-              <Route path="*" element={<Navigate to="/" />} />
-              <Route path="/auth" element={<AuthPage />} />
-              <Route path="/auth/verify" element={<VerifyPage />} />
-              {/* <Route path="/auth/signup" element={<Signup />} /> */}
-              {/* <Route
+          <QueryClientProvider client={queryclient}>
+            <RootLayout>
+              <Routes>
+                <Route path="*" element={<Navigate to="/" />} />
+                <Route path="/auth" element={<AuthPage />} />
+                <Route path="/auth/verify" element={<VerifyPage />} />
+                {/* <Route path="/auth/signup" element={<Signup />} /> */}
+                {/* <Route
                 path="/auth/resend-confirmation"
                 element={<ResendConfirmationPage />}
               /> */}
-              <Route
-                element={
-                  <Protected>
-                    <Outlet />
-                  </Protected>
-                }
-              >
-                <Route index element={<HomePage />} />
-                <Route path="/profiles/:username" element={<ProfilePage />} />
-                <Route path="/settings" element={<SettingsPage />} />
-                <Route path="/notifications" element={<NotificationsPage />} />
-              </Route>
-            </Routes>
-          </RootLayout>
+                <Route
+                  element={
+                    <Protected>
+                      <Outlet />
+                    </Protected>
+                  }
+                >
+                  <Route index element={<HomePage />} />
+                  <Route path="/profiles/:username" element={<ProfilePage />} />
+                  <Route path="/settings" element={<SettingsPage />} />
+                  <Route
+                    path="/notifications"
+                    element={<NotificationsPage />}
+                  />
+                </Route>
+              </Routes>
+            </RootLayout>
+            <ReactQueryDevtools />
+          </QueryClientProvider>
         </BrowserRouter>
       </AuthProvider>
     </ThemeProvider>
