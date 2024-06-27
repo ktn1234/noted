de3978257e8c2394e644407f0c635459c83512cb
Kevin Nguyen <ktn1234@yahoo.com>
2024-06-27 02:28:16 -0500

    :zap: :recycle: :lipstick: Refactored HomePage to use react-query and no notes feed

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,12 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { IoMdRefresh } from "react-icons/io";
+import { PostgrestError } from "@supabase/supabase-js";
+import {
+  DefaultError,
+  useMutation,
+  useQuery,
+  useQueryClient,
+} from "@tanstack/react-query";
 
 import useAuth from "../hooks/useAuth";
 
@@ -12,46 +19,102 @@ import Note from "../components/Note";
 import Modal from "../components/Modal";
 
 import supabase from "../lib/supabase";
-import { NotesJoinProfile } from "../lib/supabase/query.types";
+import {
+  NotesJoinProfile,
+  NotesJoinProfileSingle,
+} from "../lib/supabase/query.types";
 
 function HomePage(): JSX.Element {
   const { user } = useAuth();
-  const [loading, setLoading] = useState<boolean>(true);
-  const [notes, setNotes] = useState<NotesJoinProfile>([]);
+  const queryClient = useQueryClient();
 
   const [searchText, setSearchText] = useState<string>("");
   const [modalText, setModalText] = useState<string>("");
   const [showModal, setShowModal] = useState<boolean>(false);
-  const [isRefreshing, setIsRefreshing] = useState<boolean>(false);
 
-  useEffect(() => {
-    supabase
-      .from("notes")
-      .select("*, profiles (*)")
-      .order("created_at", { ascending: false })
-      .then(({ data, error }) => {
-        if (error) console.error("[ERROR] Error fetching notes", error);
-        if (data) setNotes(data);
-        setLoading(false);
+  const {
+    data: notes,
+    isFetching,
+    isLoading,
+    refetch,
+    isRefetching,
+  } = useQuery<NotesJoinProfile | null, DefaultError, NotesJoinProfile>({
+    queryKey: ["notes"],
+    initialData: [],
+    queryFn: async () => {
+      const { data, error } = await supabase
+        .from("notes")
+        .select("*, profiles (*)")
+        .order("created_at", { ascending: false });
+
+      if (error) {
+        console.error("[ERROR] Error fetching notes", error);
+        setModalText("Something went wrong while fetching notes...");
+        setShowModal(true);
+        throw error;
+      }
+
+      return data;
+    },
+  });
+
+  const { mutate: saveNote } = useMutation<
+    NotesJoinProfileSingle,
+    PostgrestError,
+    { text: string; setText: React.Dispatch<React.SetStateAction<string>> }
+  >({
+    mutationFn: async ({ text }) => {
+      const { data, error } = await supabase
+        .from("notes")
+        .insert({ text })
+        .select("*, profiles (*)")
+        .single();
+
+      if (error) throw error;
+      return data;
+    },
+    onSuccess: (_, { setText }) => {
+      queryClient.invalidateQueries({
+        queryKey: ["notes"],
       });
-  }, []);
+      setText("");
+    },
+    onError: (error) => {
+      console.error("[ERROR] Error saving note:", error);
+      setModalText(
+        "Something went wrong while saving your note. Please try again."
+      );
+      setShowModal(true);
+    },
+  });
 
-  async function handleRefreshNotes() {
-    setIsRefreshing(true);
-    const { data, error } = await supabase
-      .from("notes")
-      .select("*, profiles (*)")
-      .order("created_at", { ascending: false });
+  const { mutate: deleteNote } = useMutation<
+    undefined,
+    PostgrestError,
+    { id: number }
+  >({
+    mutationFn: async ({ id }) => {
+      const { error } = await supabase
+        .from("notes")
+        .delete()
+        .eq("id", id)
+        .single();
 
-    if (error) {
-      console.error("[ERROR] Error fetching notes", error);
-    }
-
-    if (data) {
-      setNotes(data);
-    }
-    setIsRefreshing(false);
-  }
+      if (error) throw error;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["notes"],
+      });
+    },
+    onError: (error) => {
+      console.error("[ERROR] Error deleting note:", error);
+      setModalText(
+        "Something went wrong while deleting your node. Please try again."
+      );
+      setShowModal(true);
+    },
+  });
 
   async function handleSaveNote(
     text: string,
@@ -63,115 +126,88 @@ function HomePage(): JSX.Element {
       return;
     }
 
-    try {
-      const { data, error } = await supabase
-        .from("notes")
-        .insert({ text })
-        .select("*, profiles (*)")
-        .single();
-
-      if (error) {
-        console.error("[ERROR] Error saving note:", error);
-        setModalText("Error saving note");
-        setShowModal(true);
-      }
-
-      if (data) {
-        setNotes([data, ...notes]);
-        setText("");
-      }
-    } catch (error) {
-      console.error("[ERROR] Error saving note", error);
-    }
+    saveNote({ text, setText });
   }
 
   async function handleDeleteNote(id: number) {
-    try {
-      const { error } = await supabase
-        .from("notes")
-        .delete()
-        .eq("id", id)
-        .single();
-
-      if (error) {
-        console.error("[ERROR] Error deleting note:", error);
-        return;
-      }
-
-      setNotes(notes.filter((note) => note.id !== id));
-    } catch (error) {
-      console.error("[ERROR] Error deleting note:", error);
-    }
+    deleteNote({ id });
   }
 
   function handleSearch(e: React.ChangeEvent<HTMLInputElement>) {
     setSearchText(e.target.value);
   }
 
-  if (loading || !user) return <LoadingPage />;
+  if (isLoading || !user || (isFetching && notes.length === 0)) {
+    return <LoadingPage />;
+  }
 
   const filteredNotes = notes.filter((note) =>
     note.text.toLowerCase().includes(searchText.toLowerCase())
   );
 
   return (
-    <>
-      {!loading && !!notes.length && (
-        <main className="p-3 md:p-5 flex flex-col items-center">
-          <h1 className="text-2xl text-center">All Notes</h1>
-          <section className="flex flex-col gap-2 w-full md:w-[50%]">
-            <AddNote handleSaveNote={handleSaveNote} />
-            <div className="flex justify-normal items-center mt-5">
-              <span className="w-full">
-                <Search handleSearch={handleSearch} />
-              </span>
-              <span className="mx-5">
-                <IoMdRefresh
-                  className={`cursor-pointer rounded-full p-1 border-2 border-tertiary text-secondary hover:bg-tertiary hover:text-primary dark:text-tertiary dark:border-secondary dark:hover:text-quaternary dark:hover:bg-secondary ${
-                    isRefreshing && "animate-spin"
-                  }`}
-                  size="40"
-                  onClick={handleRefreshNotes}
-                />
-              </span>
-            </div>
-            {isRefreshing && <LoadingIndicator className="mt-5" />}
-            {!isRefreshing &&
-              filteredNotes.map((note) =>
-                note.profiles?.user_id === user.id ? (
-                  <Note
-                    key={note.id}
-                    id={note.id}
-                    text={note.text}
-                    username={note.profiles!.username as string}
-                    avatar_url={note.profiles!.avatar_url as string}
-                    date={note.created_at}
-                    handleDeleteNote={handleDeleteNote}
-                  />
-                ) : (
-                  <Note
-                    key={note.id}
-                    id={note.id}
-                    text={note.text}
-                    username={note.profiles!.username as string}
-                    avatar_url={note.profiles!.avatar_url as string}
-                    date={note.created_at}
-                  />
-                )
-              )}
-          </section>
-          {showModal && (
-            <Modal
-              text={modalText}
-              closeModal={() => {
-                setShowModal(false);
-                setModalText("");
-              }}
-            />
-          )}
-        </main>
+    <main className="p-3 md:p-5 flex flex-col items-center">
+      <h1 className="text-2xl text-center">All Notes</h1>
+      {notes.length === 0 && !isFetching && (
+        <section className="flex flex-col gap-2 w-full md:w-[50%]">
+          <AddNote handleSaveNote={handleSaveNote} />
+          <span className="mt-5 text-center">
+            No notes to display. Add a note to get started!
+          </span>
+        </section>
       )}
-    </>
+      {notes.length > 0 && (
+        <section className="flex flex-col gap-2 w-full md:w-[50%]">
+          <AddNote handleSaveNote={handleSaveNote} />
+          <div className="flex justify-normal items-center mt-5">
+            <span className="w-full">
+              <Search handleSearch={handleSearch} />
+            </span>
+            <span className="mx-5">
+              <IoMdRefresh
+                className={`cursor-pointer rounded-full p-1 border-2 border-tertiary text-secondary hover:bg-tertiary hover:text-primary dark:text-tertiary dark:border-secondary dark:hover:text-quaternary dark:hover:bg-secondary ${
+                  isRefetching && "animate-spin"
+                }`}
+                size="40"
+                onClick={() => refetch()}
+              />
+            </span>
+          </div>
+          {isRefetching && <LoadingIndicator className="my-5" />}
+          {filteredNotes.map((note) =>
+            note.profiles?.user_id === user.id ? (
+              <Note
+                key={note.id}
+                id={note.id}
+                text={note.text}
+                username={note.profiles!.username as string}
+                avatar_url={note.profiles!.avatar_url as string}
+                date={note.created_at}
+                handleDeleteNote={handleDeleteNote}
+              />
+            ) : (
+              <Note
+                key={note.id}
+                id={note.id}
+                text={note.text}
+                username={note.profiles!.username as string}
+                avatar_url={note.profiles!.avatar_url as string}
+                date={note.created_at}
+              />
+            )
+          )}
+        </section>
+      )}
+      {showModal && (
+        <Modal
+          text={modalText}
+          closeModal={() => {
+            setShowModal(false);
+            setModalText("");
+          }}
+        />
+      )}
+    </main>
   );
 }
 
