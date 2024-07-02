d83d6ded0c1118412cdb8ede3717a385c32cad99
Kevin Nguyen <ktn1234@yahoo.com>
2024-07-01 20:36:46 -0500

    :sparkles: Initial implementation of note reactions

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,8 +1,32 @@
-import { useState } from "react";
+import { useState, useRef, useMemo } from "react";
 import { useNavigate, NavigateFunction } from "react-router-dom";
+import { PostgrestError } from "@supabase/supabase-js";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { FaTrash } from "react-icons/fa";
 import { TbGhost2 } from "react-icons/tb";
+import { GoSmiley } from "react-icons/go";
 import Linkify from "linkify-react";
+import EmojiPicker from "@emoji-mart/react";
+import emojiMartData from "@emoji-mart/data";
+
+import Reaction from "./Reaction";
+import useAuth from "../hooks/useAuth";
+import supabase from "../lib/supabase";
+import { ReactionsJoinProfile } from "../lib/supabase/query.types";
+
+interface UniqueEmojis {
+  [emoji: string]: string[];
+}
+
+interface Emoji {
+  id: string;
+  emoticons: string[];
+  keywords: string[];
+  name: string;
+  native: string;
+  shortcodes: string;
+  unified: string;
+}
 
 export interface NoteProps {
   id: number;
@@ -21,9 +45,13 @@ function Note({
   avatar_url,
   handleDeleteNote,
 }: NoteProps) {
+  const { user, profile } = useAuth();
+  const queryClient = useQueryClient();
   const [showUsername, setShowUsername] = useState(false);
+  const [showEmojis, setShowEmojis] = useState(false);
   const navigate = useNavigate();
   const dateString = new Date(date).toLocaleString();
+  const emojiReactionRef = useRef<HTMLDivElement | null>(null);
 
   function navigateToProfile(username: string, navigate: NavigateFunction) {
     if (window.location.pathname !== `/profiles/${username}`) {
@@ -31,13 +59,83 @@ function Note({
     }
   }
 
+  const { data: reactions } = useQuery<
+    ReactionsJoinProfile,
+    PostgrestError,
+    ReactionsJoinProfile
+  >({
+    queryKey: ["reactions", id],
+    queryFn: async () => {
+      const { data, error } = await supabase
+        .from("reactions")
+        .select("*, profiles(username)")
+        .eq("note_id", id);
+
+      if (error) throw error;
+      return data;
+    },
+  });
+
+  const uniqueEmojis = useMemo(() => {
+    const uniqueEmojis: UniqueEmojis = {};
+    if (reactions) {
+      for (let i = 0; i < reactions.length; ++i) {
+        const { emoji, profiles } = reactions[i];
+        if (uniqueEmojis[emoji] && profiles) {
+          uniqueEmojis[emoji].push(profiles.username);
+        }
+
+        if (!uniqueEmojis[emoji] && profiles) {
+          uniqueEmojis[emoji] = [profiles.username];
+        }
+      }
+    }
+
+    return uniqueEmojis;
+  }, [reactions]);
+
+  const { mutate } = useMutation<
+    undefined,
+    PostgrestError,
+    { emoji: string; note_id: number; user_id: string; username: string }
+  >({
+    mutationFn: async ({ emoji, note_id, user_id, username }) => {
+      if (uniqueEmojis[emoji] && uniqueEmojis[emoji].includes(username)) {
+        const { error } = await supabase
+          .from("reactions")
+          .delete()
+          .eq("note_id", note_id)
+          .eq("emoji", emoji)
+          .eq("user_id", user_id);
+        if (error) throw error;
+      } else {
+        const { error } = await supabase.from("reactions").insert({
+          emoji,
+          note_id,
+          user_id,
+        });
+        if (error) throw error;
+      }
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["reactions", id],
+      });
+    },
+    onError: (error) => {
+      console.error("[ERROR] Error inserting reaction", error);
+    },
+  });
+
+  if (!user || !profile) return null;
+
   return (
     <article
       key={id}
       className="flex flex-col justify-between text-primary dark:text-quaternary bg-tertiary dark:bg-secondary rounded-lg p-3 md:p-5 min-h-52"
     >
       <Linkify
-        as="span"
+        as="div"
         options={{
           target: "_blank",
           rel: "noopener noreferrer",
@@ -45,22 +143,85 @@ function Note({
             "cursor-pointer underline dark:text-quaternary hover:text-quaternary dark:hover:text-primary",
           defaultProtocol: "https",
         }}
-        className="break-words"
+        className="break-words flex-shrink-0 basis-32"
       >
         {text}
       </Linkify>
-      <div className="flex items-center justify-between">
-        <small>{dateString}</small>
-        <div className="flex items-center">
+      <div className="flex items-start justify-between gap-3">
+        <small className="whitespace-pre">{dateString}</small>
+        <div className="flex items-center gap-3">
+          {reactions && (
+            <div className="flex flex-wrap justify-end gap-2">
+              {Object.keys(uniqueEmojis).map((emoji) => {
+                return (
+                  <Reaction
+                    key={emoji}
+                    noteId={id}
+                    userId={user.id}
+                    emoji={emoji}
+                    count={uniqueEmojis[emoji].length}
+                    usernames={uniqueEmojis[emoji]}
+                  />
+                );
+              })}
+            </div>
+          )}
+          <div className="relative">
+            {showEmojis && (
+              <>
+                <div
+                  className={`absolute ${
+                    profile.username === username
+                      ? "-right-[84px]"
+                      : "-right-14"
+                  } sm:-right-10 bottom-10 py-2 bg-[#151617] rounded-lg`}
+                >
+                  <EmojiPicker
+                    data={emojiMartData}
+                    onEmojiSelect={(emoji: Emoji) => {
+                      mutate({
+                        emoji: emoji.native,
+                        note_id: id,
+                        user_id: user.id,
+                        username: profile.username,
+                      });
+                      setShowEmojis(false);
+                    }}
+                    onClickOutside={(e: PointerEvent) => {
+                      if (
+                        !emojiReactionRef.current?.contains(e.target as Node)
+                      ) {
+                        setShowEmojis(false);
+                      }
+                    }}
+                    maxFrequentRows={1}
+                    previewPosition={"none"}
+                    searchPosition={"bottom"}
+                    navPosition={"none"}
+                    skinTonePosition={"none"}
+                  />
+                </div>
+                <div className="absolute bottom-7 -right-[3px] border-l-transparent border-r-transparent border-l-[16px] border-r-[16px] border-t-[16px] border-t-[#151617]"></div>
+              </>
+            )}
+            <div ref={emojiReactionRef}>
+              <GoSmiley
+                className="cursor-pointer dark:hover:text-primary hover:text-quaternary"
+                onClick={() => setShowEmojis(!showEmojis)}
+                size={25}
+              />
+            </div>
+          </div>
           {avatar_url ? (
             <div
+              className="flex-shrink-0"
               onMouseEnter={() => setShowUsername(true)}
               onMouseLeave={() => setShowUsername(false)}
             >
               <img
                 src={avatar_url}
                 alt="Profile Picture"
-                className="w-8 h-8 rounded-full cursor-pointer"
+                className="w-8 h-8 rounded-full cursor-pointer hover:opacity-50"
                 onClick={() => navigateToProfile(username, navigate)}
               />
               {showUsername && (
@@ -71,11 +232,12 @@ function Note({
             </div>
           ) : (
             <div
+              className="flex-shrink-0"
               onMouseEnter={() => setShowUsername(true)}
               onMouseLeave={() => setShowUsername(false)}
             >
               <TbGhost2
-                className="w-8 h-8 rounded-full cursor-pointer"
+                className="w-8 h-8 rounded-full cursor-pointer hover:opacity-50"
                 size={200}
                 onClick={() => navigateToProfile(username, navigate)}
               />
@@ -88,7 +250,7 @@ function Note({
           )}
           {handleDeleteNote && (
             <FaTrash
-              className="ml-3 cursor-pointer"
+              className="cursor-pointer dark:hover:text-primary hover:text-quaternary flex-shrink-0"
               onClick={() => handleDeleteNote(id)}
             />
           )}
--- src/components/Reaction.tsx
+++ src/components/Reaction.tsx
@@ -1,22 +1,79 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useState } from "react";
+import supabase from "../lib/supabase";
+import { PostgrestError } from "@supabase/supabase-js";
+import useAuth from "../hooks/useAuth";
 
 interface ReactionProps {
+  noteId: number;
+  userId: string;
   emoji: string;
   count: number;
-  users: string[];
+  usernames: string[];
 }
 
-function Reaction({ emoji, count, users }: ReactionProps) {
+function Reaction({ noteId, userId, emoji, count, usernames }: ReactionProps) {
+  const { profile } = useAuth();
+  const queryClient = useQueryClient();
   const [showReactors, setShowReactors] = useState(false);
 
-  const reactorsText = users.reduce((prev, curr, idx, arr) => {
+  const reactorsText = usernames.reduce((prev, curr, idx, self) => {
     if (idx === 0) {
       return curr;
     }
-    if (idx === arr.length - 1) {
+    if (idx === self.length - 1) {
       return `${prev}, and ${curr}`;
     }
     return `${prev}, ${curr}`;
+  }, "");
+
+  const { mutate: deleteReaction } = useMutation<
+    undefined,
+    PostgrestError,
+    { noteId: number; emoji: string; userId: string }
+  >({
+    mutationFn: async () => {
+      const { error } = await supabase
+        .from("reactions")
+        .delete()
+        .eq("note_id", noteId)
+        .eq("emoji", emoji)
+        .eq("user_id", userId);
+
+      if (error) throw error;
+    },
+    onError: (error) => {
+      console.error("[ERROR] Error deleting reaction", error);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["reactions", noteId],
+      });
+    },
+  });
+
+  const { mutate: addReaction } = useMutation<
+    undefined,
+    PostgrestError,
+    { emoji: string; note_id: number; user_id: string }
+  >({
+    mutationFn: async () => {
+      const { error } = await supabase.from("reactions").insert({
+        emoji,
+        note_id: noteId,
+        user_id: userId,
+      });
+
+      if (error) throw error;
+    },
+    onError: (error) => {
+      console.error("[ERROR] Error inserting reaction", error);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["reactions", noteId],
+      });
+    },
   });
 
   return (
@@ -28,13 +85,28 @@ function Reaction({ emoji, count, users }: ReactionProps) {
       >
         {showReactors && (
           <>
-            <div className="absolute p-1 rounded-lg text-center bg-secondary dark:bg-primary w-32 bottom-10 -right-1/2 z-10">
+            <div className="absolute p-1 rounded-lg text-center bg-secondary text-quaternary dark:bg-primary w-32 bottom-10 -right-1/2 z-10">
               {`${reactorsText} reacted with ${emoji}`}
             </div>
-            <div className="absolute bottom-7 -right-[3px] border-l-transparent border-r-transparent border-l-[16px] border-r-[16px] border-t-[16px] border-t-primary"></div>
+            <div className="absolute bottom-8 -right-[3px] border-l-transparent border-r-transparent border-l-[16px] border-r-[16px] border-t-[16px] border-t-secondary dark:border-t-primary"></div>
           </>
         )}
-        <div className="bg-secondary text-quaternary dark:bg-primary rounded-lg px-1.5 pt-1 text-left relative top-0.5">
+        <div
+          className="flex hover:opacity-50 dark:hover:opacity-50 cursor-pointer bg-secondary dark:bg-primary text-quaternary rounded-lg px-1.5 pt-1 text-left"
+          onClick={() => {
+            if (profile && usernames.includes(profile.username)) {
+              deleteReaction({ noteId, emoji, userId });
+            }
+
+            if (profile && !usernames.includes(profile.username)) {
+              addReaction({
+                emoji,
+                note_id: noteId,
+                user_id: userId,
+              });
+            }
+          }}
+        >
           {emoji} {count}
         </div>
       </div>
